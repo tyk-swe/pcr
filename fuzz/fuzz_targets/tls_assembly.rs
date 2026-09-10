0438bac23e34742468e859e9e30ea4892bfc17d3
tyk-swe <mail@mail.tyk.sh>
2026-09-10 07:40:18 +0000

    style(fuzz): format TLS parser imports

--- fuzz/fuzz_targets/tls_assembly.rs
+++ fuzz/fuzz_targets/tls_assembly.rs
@@ -3,7 +3,9 @@
 use libfuzzer_sys::fuzz_target;
 use packetcraftr_core::protocol::application::tls::fingerprint::{Transport, ja3, ja3s, ja4};
 use packetcraftr_core::protocol::application::tls::model::Handshake;
-use packetcraftr_core::protocol::application::tls::parse::{Outcome, parse_handshake, parse_record};
+use packetcraftr_core::protocol::application::tls::parse::{
+    Outcome, parse_handshake, parse_record,
+};
 
 fuzz_target!(|data: &[u8]| {
     // 1. Record parsing
